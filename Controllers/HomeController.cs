635679fb277e61c93147f148e8f6dc09cbe47ae5
priyanshusvt-dev <154034976+priyanshusvt-dev@users.noreply.github.com>
2025-04-08 01:15:45 +0530

    Upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PriyanshuPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Nullable type parameter
+        public string Index(int? id)
+        {
+            return "id parameter ="+id;
+        }
+        public string Login(string userid,int password)
+        {
+            if (userid == "techpile" && password == 123)
+            {
+                return "Successful";
+            }
+            else
+            {
+                return "Unsucessful";
+            }
+       }
+    }
+}
